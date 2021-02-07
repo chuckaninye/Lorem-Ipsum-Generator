3529e9154a5cc2450d7efd433af2e3186df2f818
chuckaninye <chuckaninye@gmail.com>
2021-02-07 17:22:45 -0500

    Created general structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
   
-  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello world')
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+    <section className='section-center'>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>
+          Paragraphs : 
+        </label>
+        <input type='number' id='amount' value={count} onChange={(e) => setCount(e.target.value)}/>
+        <button type='submit' className='btn'>Generate</button>
+      </form>
+      <article className='lorem-text'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, non.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, non.</p>
+      </article>
+    </section>
     )
 }
 
